0c65d904c788aa3cdb7a63e1eed95777e87e1dcb
RicardoCalvoP <a01028889@tec.mx>
2025-02-05 23:35:17 -0600

    feat: presentation, projects cards

---
+++ mi-portafolio/src/presentationData.ts
@@ -0,0 +1,33 @@
+export const presentationData = {
+  name: "Ricardo Calvo",
+
+  subtitle: "Software Engineer 🚀",
+
+  aboutMe:
+    "I'm a software engineer student with a passion for web development and open-source projects. I love to learn new things and share my knowledge with others.",
+
+  img: "assets/me.jpg",
+
+  socials: [
+    { name: "GitHub", link: "https://github.com/RicardoCalvoP" },
+  ],
+
+  contacts: [
+    { name: "calvor310@gmail.com", link: "mailto:calvor310@gmail.com" },
+    { name: "+52 5586786855", link: "tel:+525586786855" },
+  ],
+
+  skills: [
+    { name: "JavaScript", icon: "https://cdn.simpleicons.org/javascript" },
+    { name: "C++", icon: "https://cdn.simpleicons.org/cplusplus" },
+    { name: "Python", icon: "https://cdn.simpleicons.org/python" },
+    { name: "TypeScript", icon: "https://cdn.simpleicons.org/typescript" },
+    { name: "Elixir", icon: "https://cdn.simpleicons.org/elixir" },
+    { name: "HTML", icon: "https://cdn.simpleicons.org/html5" },
+    { name: "CSS", icon: "https://cdn.simpleicons.org/css3" },
+    { name: "Node.js", icon: "https://cdn.simpleicons.org/nodedotjs" },
+    { name: "React", icon: "https://cdn.simpleicons.org/react" },
+    { name: "MongoDB", icon: "https://cdn.simpleicons.org/mongodb" },
+    { name: "MySQL", icon: "https://cdn.simpleicons.org/mysql" },
+  ],
+};
---
+++ mi-portafolio/src/projectsData.ts
@@ -0,0 +1,43 @@
+const img2 = "https://render.fineartamerica.com/images/rendered/small/print/8/7/break/images/artworkimages/medium/3/lebron-james-dwyane-wade-iconic-dunk-michele-haskins.jpg";
+const imgProject1 = "../assets/project1.jpg";
+
+export const projectsData = [
+  {
+    name: "Traffic Simulator",
+    date: "2024",
+    description: `
+      An AI-powered traffic simulator that uses the A* algorithm to determine the shortest path from the spawn point to the destination.
+      Using the Mesa library, we implemented a state-based system to control car behavior, allowing vehicles to avoid collisions, respect traffic lights, and follow road directions.
+      The 3D graphics were developed with WebGL, and 3D models were created using Blender.
+    `,
+    img: img2,
+    technologies: [
+      { name: "JavaScript", icon: "https://cdn.simpleicons.org/javascript" },
+      { name: "Python", icon: "https://cdn.simpleicons.org/python" },
+      { name: "HTML", icon: "https://cdn.simpleicons.org/html5" },
+      { name: "CSS", icon: "https://cdn.simpleicons.org/css3" },
+      { name: "Node.js", icon: "https://cdn.simpleicons.org/nodedotjs" },
+      { name: "WebGL", icon: "https://cdn.simpleicons.org/webgl" },
+      { name: "Blender", icon: "https://cdn.simpleicons.org/blender" },
+    ],
+  },
+  {
+    name: "Admin Dashboard",
+    date: "2024",
+    description: `
+      A web-based administrative dashboard built with React Admin to manage users, donations, and employee data.
+      The system includes role-based access control, allowing administrators to manage the information, while general users can submit donations.
+      It features a user-friendly interface with real-time data visualization, CRUD operations, and authentication using JSON Web Tokens (JWT).
+      For the backend was used Node.js and Express, with a MongoDB database data storage.
+    `,
+    img: imgProject1,
+    technologies: [
+      { name: "React", icon: "https://cdn.simpleicons.org/react" },
+      { name: "TypeScript", icon: "https://cdn.simpleicons.org/typescript" },
+      { name: "Node.js", icon: "https://cdn.simpleicons.org/nodedotjs" },
+      { name: "MongoDB", icon: "https://cdn.simpleicons.org/mongodb" },
+      { name: "Express", icon: "https://cdn.simpleicons.org/express" },
+      { name: "JWT", icon: "https://cdn.simpleicons.org/jsonwebtokens" },
+    ],
+  },
+];
