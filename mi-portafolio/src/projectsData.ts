cbaf191dac0c2adbe708ab4530da62397326f853
RicardoCalvoP <a01028889@tec.mx>
2025-02-16 23:31:24 -0600

    fixed image

--- mi-portafolio/src/projectsData.ts
+++ mi-portafolio/src/projectsData.ts
@@ -1,5 +1,7 @@
-const img2 = "https://render.fineartamerica.com/images/rendered/small/print/8/7/break/images/artworkimages/medium/3/lebron-james-dwyane-wade-iconic-dunk-michele-haskins.jpg";
-const imgProject1 = "../assets/project1.jpg";
+const img1 = "../assets/laptop.png";
+const img2 = "../assets/pc.png";
+const img3 = "../assets/canva.png";
+const imgProject1 = "../assets/project1.png";
 
 export const projectsData = [
   {
@@ -10,7 +12,7 @@ export const projectsData = [
       Using the Mesa library, we implemented a state-based system to control car behavior, allowing vehicles to avoid collisions, respect traffic lights, and follow road directions.
       The 3D graphics were developed with WebGL, and 3D models were created using Blender.
     `,
-    img: img2,
+    img: img3,
     technologies: [
       { name: "JavaScript", icon: "https://cdn.simpleicons.org/javascript" },
       { name: "Python", icon: "https://cdn.simpleicons.org/python" },
@@ -30,7 +32,26 @@ export const projectsData = [
       It features a user-friendly interface with real-time data visualization, CRUD operations, and authentication using JSON Web Tokens (JWT).
       For the backend was used Node.js and Express, with a MongoDB database data storage.
     `,
-    img: imgProject1,
+    img: img3,
+    technologies: [
+      { name: "React", icon: "https://cdn.simpleicons.org/react" },
+      { name: "TypeScript", icon: "https://cdn.simpleicons.org/typescript" },
+      { name: "Node.js", icon: "https://cdn.simpleicons.org/nodedotjs" },
+      { name: "MongoDB", icon: "https://cdn.simpleicons.org/mongodb" },
+      { name: "Express", icon: "https://cdn.simpleicons.org/express" },
+      { name: "JWT", icon: "https://cdn.simpleicons.org/jsonwebtokens" },
+    ],
+  },
+  {
+    name: "Admin Dashboard",
+    date: "2024",
+    description: `
+      A web-based administrative dashboard built with React Admin to manage users, donations, and employee data.
+      The system includes role-based access control, allowing administrators to manage the information, while general users can submit donations.
+      It features a user-friendly interface with real-time data visualization, CRUD operations, and authentication using JSON Web Tokens (JWT).
+      For the backend was used Node.js and Express, with a MongoDB database data storage.
+    `,
+    img: img3,
     technologies: [
       { name: "React", icon: "https://cdn.simpleicons.org/react" },
       { name: "TypeScript", icon: "https://cdn.simpleicons.org/typescript" },
