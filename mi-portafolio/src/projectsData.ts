44e1572da01c976f3b1151c20423f792b5c9a39e
RicardoCalvoP <a01028889@tec.mx>
2025-02-20 16:31:48 -0600

    set projects cover

--- mi-portafolio/src/projectsData.ts
+++ mi-portafolio/src/projectsData.ts
@@ -1,7 +1,7 @@
-const img1 = "../assets/laptop.png";
-const img2 = "../assets/pc.png";
-const img3 = "../assets/canva.png";
-const imgProject1 = "../assets/project1.png";
+
+const DashboardCover = "../assets/Dashboard-Project/cover.png";
+const TrafficSimulatorCover = "../assets/TrafficSimulator-Project/cover.png";
+const SpotifyAPICover = "../assets/SpotifyAPI-Project/cover.png";
 const project1 = "/AdminDashboard-Project";
 
 export const projectsData = [
@@ -13,7 +13,7 @@ export const projectsData = [
       Using the Mesa library, we implemented a state-based system to control car behavior, allowing vehicles to avoid collisions, respect traffic lights, and follow road directions.
       The 3D graphics were developed with WebGL, and 3D models were created using Blender.
     `,
-    img: img3,
+    img: TrafficSimulatorCover,
     technologies: [
       { name: "JavaScript", icon: "https://cdn.simpleicons.org/javascript" },
       { name: "Python", icon: "https://cdn.simpleicons.org/python" },
@@ -34,7 +34,7 @@ export const projectsData = [
       It features a user-friendly interface with real-time data visualization, CRUD operations, and authentication using JSON Web Tokens (JWT).
       For the backend was used Node.js and Express, with a MongoDB database data storage.
     `,
-    img: img3,
+    img: DashboardCover,
     technologies: [
       { name: "React", icon: "https://cdn.simpleicons.org/react" },
       { name: "TypeScript", icon: "https://cdn.simpleicons.org/typescript" },
@@ -47,22 +47,16 @@ export const projectsData = [
 
   },
   {
-    name: "Admin Dashboard",
+    name: "Spotify API",
     date: "2024",
     description: `
-      A web-based administrative dashboard built with React Admin to manage users, donations, and employee data.
-      The system includes role-based access control, allowing administrators to manage the information, while general users can submit donations.
-      It features a user-friendly interface with real-time data visualization, CRUD operations, and authentication using JSON Web Tokens (JWT).
-      For the backend was used Node.js and Express, with a MongoDB database data storage.
+     Use of the Spotify API to create a web application that allows users to search for songs, albums, and artists.
     `,
-    img: img3,
+    img: SpotifyAPICover,
     technologies: [
-      { name: "React", icon: "https://cdn.simpleicons.org/react" },
-      { name: "TypeScript", icon: "https://cdn.simpleicons.org/typescript" },
-      { name: "Node.js", icon: "https://cdn.simpleicons.org/nodedotjs" },
-      { name: "MongoDB", icon: "https://cdn.simpleicons.org/mongodb" },
-      { name: "Express", icon: "https://cdn.simpleicons.org/express" },
-      { name: "JWT", icon: "https://cdn.simpleicons.org/jsonwebtokens" },
+      { name: "Python", icon: "https://cdn.simpleicons.org/python" },
+      { name: "HTML", icon: "https://cdn.simpleicons.org/html5" },
+      { name: "CSS", icon: "https://cdn.simpleicons.org/css3" },
     ],
     page: project1,
 
